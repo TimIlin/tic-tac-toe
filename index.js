2dfb2a1bff7777b07e7f699e20c7ee42327fc789
Grossbauch <mishukov9042@gmail.com>
2026-02-24 20:55:50 +0500

    draw

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const container = document.getElementById('fieldWrapper');
 startGame();
 addResetListener();
 
-function startGame () {
+let n = 0
+function startGame() {
     renderGrid(3);
 }
 
-function renderGrid (dimension) {
+
+function renderGrid(dimension) {
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
@@ -26,19 +28,19 @@ function renderGrid (dimension) {
     }
 }
 
-field = [
+let field = [
     [EMPTY, EMPTY, EMPTY],
     [EMPTY, EMPTY, EMPTY],
     [EMPTY, EMPTY, EMPTY],
 ];
 
-counter = 0;
+let counter = 0;
+let end = false;
+function cellClickHandler(row, col) {
 
-function cellClickHandler (row, col) {
-    
     console.log(`Clicked on cell: ${row}, ${col}`);
-    
-    if (field[row][col] !== EMPTY) return;
+
+    if (field[row][col] !== EMPTY || end) return;
     counter += 1;
     if (counter % 2 == 1) {
         field[row][col] = CROSS;
@@ -47,72 +49,95 @@ function cellClickHandler (row, col) {
         field[row][col] = ZERO;
         renderSymbolInCell(ZERO, row, col);
     }
-    if(isWin(row,col))
+    if (isWin(row, col)) {
         alert(`победил игрок за ${counter % 2 == 1 ? CROSS : ZERO}`);
+        end = true;
+    } else if (isDraw()) {
+        alert('Победила дружба');
+        end = true;
+    }
 }
 
-function isWin(row, col){
+function isWin(row, col) {
     //строки
     let first = field[row][0];
-    for(let i = 0; i < 3; i++){
-        if(field[row][i] === EMPTY) break;
-        if(field[row][i] !== first) break;
-        if(i==2) return true;
+    for (let i = 0; i < 3; i++) {
+        if (field[row][i] === EMPTY) break;
+        if (field[row][i] !== first) break;
+        if (i == 2) return true;
     }
     //столбцы
     first = field[0][col];
-    for(let i = 0; i < 3; i++){
-        if(field[i][col] === EMPTY) break;
-        if(field[i][col] !== first) break;
-        if(i==2) return true;
+    for (let i = 0; i < 3; i++) {
+        if (field[i][col] === EMPTY) break;
+        if (field[i][col] !== first) break;
+        if (i == 2) return true;
     }
 
     //диагонали
-    if(row == col){
+    if (row == col) {
         first = field[0][0]
-        for(let i = 0; i < 3; i++){
-            if(field[i][i] === EMPTY) break;
-            if(field[i][i] !== first) break;
-            if(i==2) return true;
+        for (let i = 0; i < 3; i++) {
+            if (field[i][i] === EMPTY) break;
+            if (field[i][i] !== first) break;
+            if (i == 2) return true;
         }
     }
-    if(row + col == 2){
+    if (row + col == 2) {
         first = field[0][2]
-        for(let i = 0; i < 3; i++){
-            if(field[i][2-i] === EMPTY) break;
-            if(field[i][2-i] !== first) break;
-            if(i==2) return true;
+        for (let i = 0; i < 3; i++) {
+            if (field[i][2 - i] === EMPTY) break;
+            if (field[i][2 - i] !== first) break;
+            if (i == 2) return true;
         }
     }
 
     return false
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
+function renderSymbolInCell(symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
 
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
 
-function findCell (row, col) {
+function findCell(row, col) {
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener () {
+function addResetListener() {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler () {
-    console.log('reset!');
+function resetClickHandler() {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            field[i][j] = EMPTY;
+            renderSymbolInCell(EMPTY, i, j);
+        }
+    }
+    end = false;
+}
+
+function isDraw() {
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field[j].length; j++) {
+            if (field[i][j] === EMPTY) {
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
 
 
 /* Test Function */
 /* Победа первого игрока */
-function testWin () {
+function testWin() {
     clickOnCell(0, 2);
     clickOnCell(0, 0);
     clickOnCell(2, 0);
@@ -123,7 +148,7 @@ function testWin () {
 }
 
 /* Ничья */
-function testDraw () {
+function testDraw() {
     clickOnCell(2, 0);
     clickOnCell(1, 0);
     clickOnCell(1, 1);
@@ -136,6 +161,6 @@ function testDraw () {
     clickOnCell(2, 2);
 }
 
-function clickOnCell (row, col) {
+function clickOnCell(row, col) {
     findCell(row, col).click();
 }
